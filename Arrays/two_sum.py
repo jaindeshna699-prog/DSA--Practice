d172c99ecb2befb0a0428a5b229220e916ed9683
Deshna Jain <jaindeshna699@gmail.com>
2026-06-26 10:56:04 +0530

    Solved Two Sum

---
+++ Arrays/two_sum.py
@@ -0,0 +1,14 @@
+# Easy Level
+# Time complexity : o(n)
+# Space complexity : o(n)
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hashmap = {}
+        for i in range(len(nums)):
+            complement = target - nums[i] 
+            if complement in hashmap:
+                return [i, hashmap[complement]]
+            hashmap[nums[i]] = i
+        return []
+           
